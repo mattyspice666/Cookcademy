b216eac570cb268c45bbb33f08a565dc2efd70fa
mattyspice666 <96106005+mattyspice666@users.noreply.github.com>
2022-02-16 16:45:11 -0700

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Matthew Larrabee on 2/16/22.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
